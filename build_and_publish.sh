21dd940aff868daf826b016b6f8fd86fef5c0f52
fteutsch <florianteutsch@yahoo.de>
2024-11-16 13:03:43 +0100

    feat: add publish workflow

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -1,6 +1,8 @@
 # build
 cd streamlit_elements/frontend && npm install && npm run build
-rye build
+#rye build
+# using setup file in order to include frontend build (not included for what ever reason in case of rye build)
+python setup.py sdist bdist_wheel
 
 # publish
 rye publish
