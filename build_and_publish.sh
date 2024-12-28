f6521a24ce38994b11c87b9590103d715719e2d7
fteutsch <florianteutsch@yahoo.de>
2024-12-28 19:49:41 +0100

    feat: add start frontend server script

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -1,13 +1,20 @@
-# build
-cd streamlit_elements/frontend && npm install && npm run build
-#rye build
-# using setup file in order to include frontend build (not included for what ever reason in case of rye build)
-python setup.py sdist bdist_wheel
+# Check if the version tag has been updated
+read -p "Have you updated the version tag in streamlit_elements/version.py and pyproject.toml? (y/n): " version_update
 
-# publish
-read -p "Do you want to publish the package? (y/n): " choice
-if [[ "$choice" == [Yy]* ]]; then
-    rye publish
+if [[ "$version_update" == [Yy]* ]]; then
+    # build
+    cd streamlit_elements/frontend && npm install && npm run build
+    # rye build
+    # using setup file in order to include frontend build (not included for whatever reason in case of rye build)
+    python setup.py sdist bdist_wheel
+
+    # publish
+    read -p "Do you want to publish the package? (y/n): " publish_choice
+    if [[ "$publish_choice" == [Yy]* ]]; then
+        rye publish
+    else
+        echo "Publish step skipped."
+    fi
 else
-    echo "Publish step skipped."
+    echo "Please update the version tag in streamlit_elements/version.py and pyproject.toml before proceeding."
 fi
